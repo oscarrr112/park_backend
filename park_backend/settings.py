5bd7e69948dccd850494935c4963649334cb19b3
AoPeng <cashvjffd@163.com>
2019-12-28 23:44:43 +0800

    更改了对于跨域的支持

--- park_backend/settings.py
+++ park_backend/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
