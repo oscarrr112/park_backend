5fbea3e6b93c459ca64e5e8c96e8feadd5380168
AoPeng <cashvjffd@163.com>
2019-12-29 21:00:28 +0800

    修改了对于查询的条件的约束

--- park_backend/settings.py
+++ park_backend/settings.py
@@ -166,8 +166,8 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:8000',
     'http://localhost:8000',
     'http://114.55.255.62:8000',
-    'http://192.168.88.1',
-    'http://117.172.229.11'
+    'http://192.168.88.1:8000',
+    'http://117.172.229.11:8000'
 )
 
 CORS_ALLOW_CREDENTIALS = True  # 允许携带cookie
