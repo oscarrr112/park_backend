67fd33ff9235468a112f7ab240c2e428c2a1b262
AoPeng <cashvjffd@163.com>
2019-12-29 20:55:03 +0800

    修改了对于查询的条件的约束

--- park_backend/settings.py
+++ park_backend/settings.py
@@ -160,14 +160,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 default_icon = 'media/icon/timg.jpeg'
 default_md5 = hashlib.md5(np.array(Image.open(default_icon)))
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 # CORS 设置跨域域名
-# CORS_ORIGIN_WHITELIST = (
-#     'http://127.0.0.1:8000',
-#     'http://localhost:8000',
-#     'http://114.55.255.62:8000',
-#     'http://192.168.88.1'
-# )
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+    'http://114.55.255.62:8000',
+    'http://192.168.88.1',
+    'http://117.172.229.11'
+)
 
 CORS_ALLOW_CREDENTIALS = True  # 允许携带cookie
 
