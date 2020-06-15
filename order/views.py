d87ac488ebc69054442d176dd141d9e6d7a65f53
离猝死不远队 <1261223497@qq.com>
2020-06-15 22:10:09 +0800

    修改了对于查询的条件的约束

--- order/views.py
+++ order/views.py
@@ -77,10 +77,11 @@ class NewList(View, CommonResponseMixin):
             datetime_end = datetime(time_end.year, time_end.month, time_end.day,
                                     int(datetime_end[0]), int(datetime_end[1]))
             frequency = available_time['frequent']
+            print(datetime_start, time_start)
+            print(datetime_end, time_end)
+            print(time_start.weekday(), frequency)
+
             if time_start.weekday() + 1 in frequency and datetime_start <= time_start and time_end <= datetime_end:
-                print(datetime_start, time_start)
-                print(datetime_end, time_end)
-                print(time_start.weekday(), frequency)
                 available = True
                 break
 
