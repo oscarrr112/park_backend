a2cf4250509c2d98288d7cde588bc78f80674158
离猝死不远队 <1261223497@qq.com>
2020-06-15 18:11:13 +0800

    修改了对于查询的条件的约束

--- park_backend/settings.py
+++ park_backend/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'park_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',  # 设置数据库为mysql
-        'NAME': 'backend',  # 数据库名
-        'USER': 'root',  # 账户
-        'PASSWORD': '@LvMeng2018',  # 密码
-        'HOST': '127.0.0.1',  # 数据库的ip
-        'PORT': 3306,  # 数据库的端口
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',  # 设置数据库为mysql
+        # 'NAME': 'backend',  # 数据库名
+        # 'USER': 'root',  # 账户
+        # 'PASSWORD': '@LvMeng2018',  # 密码
+        # 'HOST': '127.0.0.1',  # 数据库的ip
+        # 'PORT': 3306,  # 数据库的端口
     }
 }
 
@@ -182,7 +182,7 @@ CORS_ALLOW_CREDENTIALS = True  # 允许携带cookie
 # 前端需要携带cookies访问后端时,需要设置
 # withCredentials: true
 
-server_url = 'http://192.168.43.230:8000'
+server_url = 'http://114.55.255.62:8000/'
 version = '/api/v1'
 
 BASE_LOG_DIR = os.path.join(BASE_DIR, "log")
